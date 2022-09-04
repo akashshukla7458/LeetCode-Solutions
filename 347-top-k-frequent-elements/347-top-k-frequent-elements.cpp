3ac2edf32f37ba4795dc21a42465dc218624af46
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2022-09-04 19:58:13 +0530

    Time: 33 ms (24.74%), Space: 13.7 MB (73.00%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+      unordered_map<int,int>m;
+		vector<pair<int,int>> a;
+		for(int i=0;i<nums.size();++i){
+			m[nums[i]]+=1;
+		}
+		for(auto x:m){
+			a.push_back({x.second,x.first});
+		}
+		sort(a.rbegin(),a.rend());
+		vector<int>ans(k);
+		for(int i=0;i<k;++i){
+			ans[i] = a[i].second;
+		}
+		return ans;
+    }
+};
