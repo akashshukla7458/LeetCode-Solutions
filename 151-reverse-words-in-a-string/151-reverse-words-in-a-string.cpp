7d997021a7d82e4777f668cb0eeeee4336d88971
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2022-09-09 17:32:49 +0530

    Time: 23 ms (20.12%), Space: 21 MB (14.66%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream is(s);
+        string str;
+        string ans="";
+        int i=0;
+        while(is>>str){
+            if(i==0){
+                ans=str+ans;
+                
+            }
+            else{
+                ans=str+ " "+ans;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
