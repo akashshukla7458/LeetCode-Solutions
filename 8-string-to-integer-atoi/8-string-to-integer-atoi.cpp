8e3151fa9c0ef8e1b4e7382380643401105e71ac
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2022-09-10 19:29:00 +0530

    Time: 0 ms (100.00%), Space: 7 MB (53.49%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+        int myAtoi(string s)
+        {
+            int value {};
+            stringstream ss
+            {
+                s
+            };
+
+            ss >> value;
+
+            return value;
+        }
+};
