5f68ae7bed6cf767c4f39326869892536446d295
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2022-09-26 20:07:37 +0530

    Time: 122 ms (69.40%), Space: 86.6 MB (5.01%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+        int f(vector<int> &nums, int targ, int i, vector<vector< long long int>> &dp)
+        {
+            if (i == 0)
+            {
+                if (targ % nums[i] == 0)
+                    return dp[i][targ] = targ / nums[i];
+                else
+                    return 1e9;
+            }
+
+            if (dp[i][targ] != -1) return dp[i][targ];
+
+            int not_take = 0 + f(nums, targ, i - 1, dp);
+            int take = INT_MAX;
+
+            if (nums[i] <= targ)
+            {
+                take = 1 + f(nums, targ - nums[i], i, dp);
+            }
+
+            return dp[i][targ] = min(take, not_take);
+        }
+
+    int coinChange(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        vector<vector < long long int>> dp(n + 1, vector < long long int > (target + 1, -1));
+        long long int ans = f(nums, target, n - 1, dp);
+        if (ans >= 1e9) return -1;
+        return ans;
+    }
+};
