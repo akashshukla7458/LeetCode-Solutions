fa14ffbcfce06b7cd8f60fd1c0a2d8f9a207bb62
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-05-11 09:52:10 +0530

    Time: 9 ms (92.82%) | Memory: 9.8 MB (88.04%) - LeetSync

--- uncrossed-lines/uncrossed-lines.cpp
+++ uncrossed-lines/uncrossed-lines.cpp
@@ -1,48 +1,15 @@
-// class Solution {
-// public:
-//     int dp[501][501]; // increased array size by 1
-//     int solve(int i, int j, vector<int>& nums1, vector<int>& nums2){
-//         if (i == nums1.size() || j == nums2.size()) return 0;
-//         if (dp[i][j] != 0) return dp[i][j];
-
-//         int taken = 0;
-//         if (nums1[i] == nums2[j]) {
-//             taken = 1 + solve(i+1, j+1, nums1, nums2);
-//         }
-//         int not_taken1 = solve(i+1, j, nums1, nums2);
-//         int not_taken2 = solve(i, j+1, nums1, nums2);
-//         int not_taken = max(not_taken1, not_taken2);
-//         dp[i][j] = max(taken, not_taken);
-//         return dp[i][j];
-//     }
-
-//     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-//         memset(dp, 0, sizeof(dp));
-//         return solve(0, 0, nums1, nums2);
-//     }
-// };
 
 
 
 class Solution {
 public:
-    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); // initialize 2D DP array with zeros
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (nums1[i-1] == nums2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + 1; // diagonal + 1
-                }
-                else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]); // max of top and left
-                }
-            }
-        }
-
-        return dp[n][m]; // return the last element in the 2D DP array
+        int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+        int m = A.size(), n = B.size(), dp[m+1][n+1];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= m; ++i)
+            for (int j = 1; j <= n; ++j)
+                dp[i][j] = A[i - 1] == B[j - 1] ? dp[i - 1][j - 1] + 1 : max(dp[i][j - 1], dp[i - 1][j]);
+        return dp[m][n];
     }
 };
 
