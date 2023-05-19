33f9c8c56668d212e9d6ee7a6d701a0376161918
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-05-19 11:10:22 +0530

    Time: 19 ms (94.92%) | Memory: 13.5 MB (82.63%) - LeetSync

---
+++ is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+bool bi(int i, vector<vector<int>>&graph, vector<int>&color){
+    if(color[i]==-1) color[i]=1;
+    for(auto it: graph[i]){
+        if(color[it]==-1){ 
+           color[it]=1-color[i];
+           if(!bi(it,graph,color)) return false;
+           }
+        else if(color[it]== color[i]) return false;
+    }
+    return true;
+}
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n= graph.size();
+        vector<int>color(n,-1);
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(!bi(i,graph,color)) return false;
+            }
+        }
+        return true;
+    }
+};
