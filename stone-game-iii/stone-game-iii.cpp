afdbb1aac891891c599c1ebe30123b0eda8b5632
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-05-27 18:35:17 +0530

    Time: 265 ms (61.37%) | Memory: 132.2 MB (81.04%) - LeetSync

--- stone-game-iii/stone-game-iii.cpp
+++ stone-game-iii/stone-game-iii.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-int solve(vector<int>&piles, int i, vector<int>&dp){
-int n= piles.size();
-if(i>=n) return 0;
-if(dp[i]!=INT_MAX) return dp[i];
 
-int takeone= piles[i] - solve( piles,i+1,dp);
-int taketwo= INT_MIN, takethree= INT_MIN;
-if(i+1<n) taketwo= piles[i]+ piles[i+1] - solve(piles,i+2,dp);
-if(i+2<n) takethree= piles[i]+ piles[i+1]+ piles[i+2] - solve( piles,i+3,dp);
-return dp[i]=max({takeone,taketwo,takethree});
-
-}
     string stoneGameIII(vector<int>&piles) {
-      vector<int>dp(piles.size(),INT_MAX);
-      int val= solve(piles,0,dp);
+     
+     int n= piles.size();
+      vector<int>dp(n+1,0);
+     
+      for(int i =n-1;i>=0;i--){
+        int takeone= piles[i] - dp[i+1];
+       int taketwo= INT_MIN, takethree= INT_MIN;
+        if(i+1<n) taketwo= piles[i]+ piles[i+1] - dp[i+2];
+        if(i+2<n) takethree= piles[i]+ piles[i+1]+ piles[i+2] - dp[i+3];
+      dp[i]=max({takeone,taketwo,takethree});
+      }
+      int val=dp[0];
+
+
       if(val>0) return "Alice";
       else if(val<0) return "Bob";
       else return "Tie";
