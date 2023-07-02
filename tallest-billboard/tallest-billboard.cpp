fc31515e3c65b4adff073be7a019a78a9c9c237a
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-07-02 16:50:21 +0530

    Time: 1120 ms (5.31%) | Memory: 215.9 MB (5.86%) - LeetSync

--- tallest-billboard/tallest-billboard.cpp
+++ tallest-billboard/tallest-billboard.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+vector<unordered_map<int,int>>dp;
+int solve(int i, vector<int>&v, int d){
+    if(i==v.size()){
+        if(d==0) return 0;
+        return INT_MIN;
+    }
+
+    if(dp[i].find(d)!=dp[i].end()) return dp[i][d];
+    int x= solve(i+1,v,d);
+    int y= v[i]+ solve(i+1,v, d+ v[i]);
+    int z= solve(i+1, v, d-v[i]);
+
+    return dp[i][d]= max({x,y,z});
+}
    int tallestBillboard(vector<int>& rods) {
-        unordered_map<int, int> dp;
-        dp[0] = 0;
-        for (int x : rods) {
-            unordered_map<int, int> cur(dp);
-            for (auto it: cur) {
-                int d = it.first;
-                dp[d + x] = max(dp[d + x],cur[d]);
-                dp[abs(d - x)] = max(dp[abs(d - x)], cur[d] + min(d, x));
-            }
-        }
-        return dp[0];
+    dp.assign(21, {});
+     return solve(0,rods,0);
     }
 };
