c9c2a35032b7e612f418bef6743be5057e4b6933
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-07-24 21:59:00 +0530

    Time: 5 ms (36.81%) | Memory: 7.2 MB (60.45%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+void solve(vector<int>&nums, int i , vector<int>&temp, vector<vector<int>>&ans){
+      ans.push_back(temp);
+      for(int j=i;j<nums.size();j++){
+          temp.push_back(nums[j]);
+          solve(nums,j+1,temp, ans);
+          temp.pop_back();
+      }
+
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>temp;
+        vector<vector<int>>ans;
+        solve(nums,0,temp,ans);
+        return ans;
+    }
+};
