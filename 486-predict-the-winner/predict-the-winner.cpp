bc78fef5a10075f01f85b477089b7c712d44e624
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-07-28 17:05:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (75.78%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-
+int dp[21][21];
 int solve(vector<int>&nums, int i, int j){
     if(i>j) return 0;
     if(i==j) return nums[i];
+    if(dp[i][j]!=-1) return dp[i][j];
     int curr_score= max( 
        ( nums[i]+ min(solve(nums,i+2,j), solve(nums,i+1,j-1))),
     
        ( nums[j]+ min(solve(nums,i+1,j-1), solve(nums,i,j-2)))
     );
-    return curr_score;
+    return dp[i][j]=curr_score;
 }
     bool PredictTheWinner(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
         int score= solve(nums,0,nums.size()-1);
         int max_score=0;
         for(auto it: nums) max_score+=it;
