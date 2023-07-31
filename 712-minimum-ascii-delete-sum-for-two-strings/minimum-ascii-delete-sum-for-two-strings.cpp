78726e9c71b154fbf32ab3d18b64a0f1395631b6
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-07-31 13:58:27 +0530

    Time: 37 ms (80.65%) | Memory: 15.3 MB (41.16%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-vector<vector<int>>dp;
-int deadend( string &s, int i){
-int sum=0;
- for(;i<s.length();i++)  sum+= (int)s[i];
- return sum;
-}
-int solve(string &s1, string &s2, int i, int j ){
-int n= s1.length(), m= s2.length(), sum=0;
-if(i==n || j==m){
-    if(i==n && j==m) return 0;
-    return (i==n)? deadend(s2,j) : deadend(s1,i);
-}
-if(dp[i][j]!=-1) return dp[i][j];
-if(s1[i]==s2[j]) sum= solve(s1,s2,i+1,j+1);
-
-else{
-    sum= min({ solve(s1,s2,i+1,j)+ int(s1[i])
-               , solve(s1,s2,i,j+1)+ int(s2[j])
-               });
-}
-return dp[i][j]= sum;
-
-
-   
-}
-    int minimumDeleteSum(string s1, string s2) {
-      int n= s1.length() , m= s2.length();
-      dp.assign(n+1,vector<int>(m+1,-1));
-      return solve(s1,s2,0,0);
+     int minimumDeleteSum(string s1, string s2) {
+        int n= s1.length(), m= s2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        //dp initialize
+        for(int i=1;i<=n;i++)
+           dp[i][0]= dp[i-1][0]+ int(s1[i-1]);
+        for(int j=1; j<=m;j++)
+          dp[0][j]= dp[0][j-1]+ int(s2[j-1]);
+        
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1])
+                  dp[i][j]= dp[i-1][j-1];
+                else{
+                    dp[i][j]= min(
+                        dp[i-1][j]+ s1[i-1], dp[i][j-1]+ s2[j-1]
+                    );
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
