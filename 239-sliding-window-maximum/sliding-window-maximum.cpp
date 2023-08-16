10e397214c9e9c7e37e2f751d2faa4c6009fcfd3
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-08-16 10:26:27 +0530

    Time: 215 ms (82.76%) | Memory: 134.8 MB (36.59%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>q;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            //out of bounds
+            while(!q.empty() && q.front()==i-k) q.pop_front();
+            while(!q.empty() && nums[i]> nums[q.back()]) q.pop_back();  //dec montonic queue
+            q.push_back(i);
+
+            if(i>=k-1) ans.push_back(nums[q.front()]);
+        }
+        return ans;
+    }
+};
