5c26cf6b9f867cea04fc86c8446c2f1ed05a655a
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-08-21 17:35:18 +0530

    Time: 4 ms (81.77%) | Memory: 13.1 MB (40.35%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
-vector<vector<int>>dp;
-int solve(vector<int>&prices, int i, bool buy){
-    int n= prices.size();
-    if(i==n) return 0;
-    if(dp[i][buy]!=-1) return dp[i][buy];
-    int profit;
-    if(buy==0){
-        profit= max(solve(prices,i+1,0) , -prices[i]+ solve(prices,i+1,1));
-    }
-    if(buy==1){
-        profit= max(solve(prices,i+1,1), prices[i]+ solve(prices, i+1,0));
-    }
-   return dp[i][buy]= profit;
-
-}
-    int maxProfit(vector<int>& prices) {
-        int n= prices.size();
-        dp.resize(n+1, vector<int>(2,-1));
-        if(n==0) return 0;
-        return solve(prices, 0, 0);
+    int maxProfit(vector<int>& P) {
+        int profit = 0;
+        for(int i = 1; i < size(P); i++)
+            if(P[i] > P[i-1])              // yesterday was valley, today is peak
+                profit += P[i] - P[i-1];   // buy yesterday, sell today
+        return profit;
     }
 };
