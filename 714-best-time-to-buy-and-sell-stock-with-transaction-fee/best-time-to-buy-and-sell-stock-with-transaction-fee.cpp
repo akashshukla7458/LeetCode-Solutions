485886a71f23be52d2e9bb019ba54b9400b3f9ed
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-08-22 00:41:18 +0530

    Time: 65 ms (99.73%) | Memory: 55 MB (64.35%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+         int buy= INT_MIN;
+         int sell=0;
+         for(int p:prices){
+             buy= max(buy, sell- p);
+             sell= max(sell, buy+ p -fee);
+         }
+         return sell;
+    }
+};
