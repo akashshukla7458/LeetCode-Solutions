61a0e706c37e8a10ee8d806c8713dd3aad5ec91a
Akash Shukla <85392276+akashshukla7458@users.noreply.github.com>
2023-09-06 01:31:24 +0530

    Time: 0 ms (100.00%) | Memory: 5.8 MB (92.97%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+      if(n>9){
+        int res=0;
+        while(n>0){
+            res+= (n%10)* (n%10);
+            n/=10;
+        }
+        return isHappy(res);
+    }
+    if(n==1 || n==7) return true;
+    else return false;
+        
+    }
+};
